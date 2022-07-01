5a2f63be6c6e1219e14d4db77e6d64e532778d36
Wijayarathna P.G.R.C <it18141948@my.sliit.lk>
2022-07-01 12:46:59 +0530

    Add Update Whrn Multiple Auth

--- src/main/java/com/example/demo/controller/RegistrationController.java
+++ src/main/java/com/example/demo/controller/RegistrationController.java
@@ -8,17 +8,19 @@ package com.example.demo.controller;
 import com.example.demo.DTO.RegistrationRequestDTO;
 import com.example.demo.business.RegistrationServiceBO;
 import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping(path = "api/v1/registration")
 @AllArgsConstructor
+@Slf4j
 public class RegistrationController { // Registration Controller
 
     private final RegistrationServiceBO registrationServiceBO; // Reference to RegistrationServiceBO Service
 
     @PostMapping  // Register User Json Object
-   // {   "firstName":"Rivindu",  "lastName" :"Wijayarathna", "email" : "gemzone0@gmail.com", "password" : "1234" }
+    // {   "firstName":"Rivindu",  "lastName" :"Wijayarathna", "email" : "gemzone0@gmail.com", "password" : "1234" }
     public String register(@RequestBody RegistrationRequestDTO request) {
         return registrationServiceBO.register(request);
     }
@@ -26,7 +28,14 @@ public class RegistrationController { // Registration Controller
     @GetMapping(path = "confirm")
     // Confirmation email link will take token, name and email
     public String confirm(@RequestParam("token") String token, @RequestParam("name") String name, @RequestParam("email") String email) {
-        return registrationServiceBO.confirmToken(token, name, email);// Method to Confirm token is match to the token stored in the database
+        String confirm = null;
+        try {
+            confirm = registrationServiceBO.confirmToken(token, name, email);// Method to Confirm token is match to the token stored in the database
+        } catch (IllegalStateException e) {
+            log.info("Token Not Found");
+        }
+
+        return confirm;
     }
 
 }// End Controller
